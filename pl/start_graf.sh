9380954931a33bc783fed90410909263823fb430
Karl Schroeder <subrock@users.noreply.github.com>
2023-11-26 05:51:40 -0800

    Add files via upload

---
+++ pl/start_graf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo -n "Starting Grafana..."
+GF_AUTH_DISABLE_LOGIN_FORM=true GF_AUTH_ANONYMOUS_ENABLED=true GF_AUTH_ANONYMOUS_ORG_ROLE=Admin GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-polystat-panel,grafana-image-renderer grafana-server --config=/etc/grafana/grafana.ini --homepath /usr/share/grafana web &
+echo 
+Sleep 20
+echo Done
+echo 
+
